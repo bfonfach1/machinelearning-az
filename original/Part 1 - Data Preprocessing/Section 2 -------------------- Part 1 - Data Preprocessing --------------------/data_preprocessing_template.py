d0f4401ccc104afd2319efb734045fe891f81902
bfonfach1 <bastianfonfach@gmail.com>
2024-06-27 16:43:16 -0400

    Update data_preprocessing_template.py

--- original/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
+++ original/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
@@ -10,22 +10,18 @@ Created on Wed Feb 27 19:43:11 2019
 
 # Cómo importar las librerías
 import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
+import matplotlib.pyplot as plt #para llamar una sublibreria es la libreria.la sublibreriaq se quiere
+import pandas as pd #libreria para cargar datos 
 
-# Importar el data set
-dataset = pd.read_csv('Data.csv')
-X = dataset.iloc[:, :-1].values
+#Importar el dataset
+dataset = pd.read_csv('Data.csv') #pd es como llame a pandas
+
+
+#--> la variable X representa las variables independientes y la variables Y las variables dependientes
+
+X = dataset.iloc[:, :-1].values # la funcion iloc sirve para identificar los elementos de un dataset por posicion. El  desde:hasta, si no quiero la ultima es [ :-1].value
 y = dataset.iloc[:, 3].values
 
-
-# Dividir el data set en conjunto de entrenamiento y conjunto de testing
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-
-
-# Escalado de variables
-from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
+#tratamiento de los NAs o valores que no estan en el data frame
+from sklearn.preprocessing import Imputer  #imputer sirve para iputar los datos que faltan, usamos from ___- import solo la funcion que queremos de la libreria}
+imputer = Imputer(missing_values = 'nan', strategy = 'mean', axis = 0) #axis=0 se le aplica a la columna 
